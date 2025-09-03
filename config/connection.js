d4e78c6a68ab5a94aa7ff42d9f0ef6a21e82b81d
Cheyenne <cheyennejjohnson96@gmail.com>
2025-09-03 16:39:01 -0400

    mogoose conect

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
+mongoose.set("strictQuery", true);
 
-async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
+ mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('MongoDB connected ✅');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
-  }
-}
-
-module.exports = connectDB;
+  
+module.exports = mongoose.connection;
