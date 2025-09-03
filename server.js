3808e318f0fb0d255868ed7d35da964a3fde3e1e
Cheyenne <cheyennejjohnson96@gmail.com>
2025-09-03 16:35:41 -0400

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const db = require("./config/connection");
 
 dotenv.config();
 const app = express();
@@ -8,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.send('API is running 🚀');
+app.use('/api', require("./routes/api"));
+ 
+db.once ("open", () => {
+  app.listen(PORT, () =>  console.log("Server running on http://localhost:${PORT}"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port PORT`);
-});
